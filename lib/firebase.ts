2c6280d73e288821e3aa09eb45457e736a78d8c4
Gan2205 <uppalaganeshsivaram@gmail.com>
2026-02-15 14:13:35 +0530

    chore: externalize firebase config to env vars

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyACODkW91PsTouAZcp4yAGbPywCRyTdOww",
-    authDomain: "health-4fa4c.firebaseapp.com",
-    projectId: "health-4fa4c",
-    storageBucket: "health-4fa4c.firebasestorage.app",
-    messagingSenderId: "629204711105",
-    appId: "1:629204711105:web:8de75d4d3f111e98984640",
-    measurementId: "G-G9WMW8369X"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
